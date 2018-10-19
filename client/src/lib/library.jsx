44fdfac69f619115f355e17c3f3a65a7fcca22c7
hifromkevin <lam.kevin86@gmail.com>
2018-10-18 19:18:25 -0700

    Reassigns helpers to library

--- client/src/lib/library.jsx
+++ client/src/lib/library.jsx
@@ -1,8 +1,26 @@
   let showSymbols = (num, symbol) => {
     var useSymbol = (symbol === '$') ? '$' : '* ';
     //replace % with empty stars
-    var fillSymbol = (symbol === '$') ? '' : '% ';
-    return Array(5).fill(fillSymbol).fill(useSymbol, 0, num).join('');
+    var fillSymbol = (symbol === '$') ? '' : '• ';
+    var heyArray = Array(5).fill(fillSymbol).fill(useSymbol, 0, num).join('');
+    return Array(5).fill(fillSymbol).fill(useSymbol, 0, num);
   }
 
 exports.showSymbols = showSymbols;
+
+
+/* 
+
+	Looking for a method to inject HTML using a method
+
+  let showSymbols = (num, symbol) => {
+    var useSymbol = (symbol === '$') ? '$' : '<span className="mainInfo__rating__stars__true">*</span> ';
+    //replace % with empty stars
+    var fillSymbol = (symbol === '$') ? '' : '<span className="mainInfo__rating__stars__false">*</span> ';
+    var heyArray = Array(5).fill(fillSymbol).fill(useSymbol, 0, num).join('');
+    return Array(5).fill(fillSymbol).fill(useSymbol, 0, num);
+  }
+
+exports.showSymbols = showSymbols;
+
+*/
