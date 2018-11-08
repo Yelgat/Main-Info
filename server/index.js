949d655bfc0a13e11f15a4cde6fd2055d35be5a1
hifromkevin <lam.kevin86@gmail.com>
2018-11-08 08:55:23 -0800

    Adds database connection

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
 const express = require('express');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const port = process.env.PORT || 3005;
 
 const app = express();
+const port = process.env.PORT || 3005;
+const DB_ADDRESS = process.env.DB_ADDRESS || 'localhost:27017';
+
+const db = require('../db/model.js');
+
+mongoose.connect(`mongodb://${DB_ADDRESS}/Yelgat_Main`, {}, console.log);
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+app.get('/r/:namecitynumber', (req, res) => {
+	db.findStore(req.params.namecitynumber, (err, results) => {
+		(err) ? res.send(404).send(`Cannot find store ${req.params.namecitynumber}`) : res.json(results);
+	});
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
