a743fa03ba724203a8f8f48b0ae43cb7717d075e
hifromkevin <lam.kevin86@gmail.com>
2018-11-08 08:55:52 -0800

    Adds connection to MongoDB

---
+++ db/model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose'); // Do I need this???
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection Error: '));
+db.once('open', () => {
+	console.log('Yelgat Main database HELLA connected');
+});
+
+const yelgatMainSchema = mongoose.Schema({
+	id: { type: Number, unique: true },
+	companyName: String,
+	type: String,
+	numberOfStars: Number,
+	numberOfReviews: Number,
+	numberOfDollarSigns: Number,
+	typeOfCompany: String
+});
+ 
+const YelgatMainModel = mongoose.model('Yelgat_Main', yelgatMainSchema);
+
+const findAll = (cb) => {
+	YelgatMainModel.find({}).exec(cb);
+}
+
+const findStore = (store, cb) => {
+	YelgatMainModel.find(store).exec(db);
+}
+
+exports.findAll = findAll;
+exports.findStore = findStore;
